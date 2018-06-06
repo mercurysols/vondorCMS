589052cf01fd481f089a39850a27a2af2d6824b4
mohanedmagdii <mohaned-magdy@hotmail.com>
2018-06-06 02:26:16 +0200

    initial

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ module.exports = {
     },
 
     server : {
-        host:'http://127.0.0.1',
-        port : 1234
+        host:'https://api-5514.herokuapp.com/',
+        port : 37397
     },
     dirPath : {
         model:'../models',
